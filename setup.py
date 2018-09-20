5afc029707aa2bdd62159096ae2201fdf5c0b234
Tom de Geus <tom@geus.me>
2018-09-20 14:47:38 +0200

    Less case sensitivity

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 
+import os
+
 from setuptools                 import setup
 from setuptools.command.install import install
 
@@ -26,6 +28,11 @@ class PostInstallCommand(install):
 
 # --------------------------------------------------------------------------------------------------
 
+if os.path.isdir('GooseMPL'): packages = ['GooseMPL']
+else                        : packages = ['goosempl']
+
+# --------------------------------------------------------------------------------------------------
+
 setup(
   name              = 'GooseMPL',
   version           = '0.2.6',
@@ -37,7 +44,7 @@ setup(
   long_description  = '',
   license           = 'MIT',
   install_requires  = ['matplotlib>=2.0.0', 'numpy>=1.0.0'],
-  packages          = ['GooseMPL'],
+  packages          = packages,
   cmdclass          = {'install': PostInstallCommand},
 )
 
