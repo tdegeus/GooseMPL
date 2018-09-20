03d7f08f6521f84b9f4afdd58727d4016eedda8a
Tom de Geus <tom@geus.me>
2018-09-20 12:18:43 +0200

    Updated documentation and example

--- docs/examples/goosempl/histogram_powerlaw.py
+++ docs/examples/goosempl/histogram_powerlaw.py
@@ -19,29 +19,7 @@ data = distribution()
 
 fig, axes = plt.subplots(ncols=3, figsize=(24,8))
 
-# --- histogram ---
-
-P,x = gplt.histogram(data, bins=41, density=True, return_edges=False)
-
-axes[0].plot(x,P,marker='o', linestyle='none', markersize=5., color='k')
-
-# --- histogram_log ---
-
-P,x = gplt.histogram_log(data, bins=41, density=True, return_edges=False)
-
-axes[1].plot(x,P,marker='o', linestyle='none', markersize=5., color='b')
-
-# --- histogram_uniform ---
-
-P,x = gplt.histogram_uniform(data, bins=41, density=True, return_edges=False)
-
-axes[2].plot(x,P,marker='o', linestyle='none', markersize=5., color='r')
-
-# --- axes settings ---
-
-axes[0].set_title(r'histogram')
-axes[1].set_title(r'histogram\_log')
-axes[2].set_title(r'histogram\_uniform')
+# configure axes
 
 for ax in axes:
 
@@ -54,8 +32,38 @@ for ax in axes:
   ax.set_xlim([10**0   , 10**3])
   ax.set_ylim([10**(-3), 10**0])
 
-  gplt.plot_powerlaw(exp=-1.3, startx=.4, starty=.5, width=.2, axis=ax, units='relative')
-  gplt.text(.5, .4, r'$-1.3$', axis=ax, units='relative')
+# histogram
+
+P,x = gplt.histogram(data, bins=41, density=True, return_edges=False)
+
+axes[0].plot(x,P,marker='o', linestyle='none', markersize=5., color='k')
+
+# histogram_log
+
+P,x = gplt.histogram_log(data, bins=41, density=True, return_edges=False)
+
+axes[1].plot(x,P,marker='o', linestyle='none', markersize=5., color='b')
+
+# histogram_uniform
+
+P,x = gplt.histogram_uniform(data, bins=41, density=True, return_edges=False)
+
+axes[2].plot(x,P,marker='o', linestyle='none', markersize=5., color='r')
+
+# add titles
+
+axes[0].set_title(r'histogram')
+axes[1].set_title(r'histogram\_log')
+axes[2].set_title(r'histogram\_uniform')
+
+# annotate powerlaw in different ways
+
+for ax in axes:
+
+  gplt.grid_powerlaw(exp=-1.3, axis=ax)
+
+  gplt.plot_powerlaw(               exp=-1.3, startx=.4, starty=.5, width=.2, axis=ax, units='relative')
+  gplt.annotate_powerlaw(r'$-1.3$', exp=-1.3, startx=.4, starty=.5, width=.2, axis=ax, units='relative', rx=.55)
 
 plt.savefig('histogram_powerlaw.svg')
 plt.show()
