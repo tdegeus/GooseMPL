b8fda71584fa0edc37590b8eb9709904f8f684ac
Tom de Geus <tdegeus@users.noreply.github.com>
2021-06-29 14:13:17 +0200

    Adding integer bin-edges to public API (formerly only integrated in histogram_bin_edges) + minor bugfix (#25)

--- test/main.py
+++ test/main.py
@@ -31,6 +31,27 @@ class Test_fit_powerlaw(unittest.TestCase):
         self.assertTrue(np.isclose(exponent, 3.4))
 
 
+class Test_histogram_bin_edges_integer(unittest.TestCase):
+
+    def test_front(self):
+
+        a = [0, 0.5, 1.5, 2.5]
+        b = [0, 1.5, 2.5]
+        self.assertTrue(np.allclose(gplt.histogram_bin_edges_integer(a), b))
+
+    def test_middle(self):
+
+        a = [0, 1.5, 1.6, 2.5]
+        b = [0, 1.6, 2.5]
+        self.assertTrue(np.allclose(gplt.histogram_bin_edges_integer(a), b))
+
+    def test_back(self):
+
+        a = [0, 1.5, 2.5, 2.6]
+        b = [0, 1.5, 2.6]
+        self.assertTrue(np.allclose(gplt.histogram_bin_edges_integer(a), b))
+
+
 if __name__ == '__main__':
 
     unittest.main()
