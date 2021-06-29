e0f29f5b890e49ecced9a5b80f4bf323dc1d5d73
Tom de Geus <tom@geus.me>
2021-06-29 13:50:00 +0200

    Adding extra tests

--- test/main.py
+++ test/main.py
@@ -6,11 +6,27 @@ import numpy as np
 
 class Test_fit_powerlaw(unittest.TestCase):
 
-    def test_basic(self):
+    def test_prefactor_exponent(self):
 
         x = np.linspace(0, 1, 1000)
         y = 1.2 * x ** 3.4
         prefactor, exponent = gplt.fit_powerlaw(x, y)
+        self.assertTrue(np.isclose(prefactor, 1.2))
+        self.assertTrue(np.isclose(exponent, 3.4))
+
+    def test_prefactor(self):
+
+        x = np.linspace(0, 1, 1000)
+        y = 1.2 * x ** 3.4
+        prefactor, exponent = gplt.fit_powerlaw(x, y, exponent=3.4)
+        self.assertTrue(np.isclose(prefactor, 1.2))
+        self.assertTrue(np.isclose(exponent, 3.4))
+
+    def test_exponent(self):
+
+        x = np.linspace(0, 1, 1000)
+        y = 1.2 * x ** 3.4
+        prefactor, exponent = gplt.fit_powerlaw(x, y, prefactor=1.2)
         self.assertTrue(np.isclose(prefactor, 1.2))
         self.assertTrue(np.isclose(exponent, 3.4))
 
