c85db8733df7c2f95815f24372b37af4b6f712b0
Tom de Geus <tom@geus.me>
2022-08-31 14:40:02 +0200

    [docs] Minor clean-up configuration

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ import sys
 sys.path.insert(0, os.path.abspath(".."))
 
 project = "GooseMPL"
-copyright = "2017, Tom de Geus"
+copyright = "2017-2022, Tom de Geus"
 author = "Tom de Geus"
 
 autodoc_type_aliases = {
@@ -20,6 +20,4 @@ extensions = [
 ]
 
 html_theme = "furo"
-templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-html_static_path = ["_static"]
